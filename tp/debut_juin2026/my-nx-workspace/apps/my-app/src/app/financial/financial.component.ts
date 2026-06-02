4ef0ad4643d2f02ad6804e551f380b4d82bdb494
Didier Defrance <didier@d-defrance.fr>
2026-06-02 10:55:41 +0200

    s

--- tp/debut_juin2026/my-nx-workspace/apps/my-app/src/app/financial/financial.component.ts
+++ tp/debut_juin2026/my-nx-workspace/apps/my-app/src/app/financial/financial.component.ts
@@ -1,6 +1,12 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { FinancialService } from '../common/service/financial.service';
 import { FormsModule } from '@angular/forms';
+
+class Loan {
+  constructor(public amount:number=0,
+              public nbMonth:number=0,
+               public annualRate:number=0){}
+}
 
 
 @Component({
@@ -15,6 +21,18 @@ export class FinancialComponent {
   nbMonth=signal(120);
   amount=signal<number>(100000);
   annualRate=signal<number>(2.5);
+
+  loan = new Loan(this.amount() , this.nbMonth(), this.annualRate());
+  sLoan = signal<Loan>(this.loan);
+
+  logEffectAmount = effect(()=>{
+    console.log("nouvelle valeur de amount=" + this.amount())
+    this.loan.amount=this.amount();
+    //this.sLoan.set(this.loan); //ne fonctionne pas  au sens signal sLoan considéré comme inchangé et pas de réactualisation
+    this.sLoan.set(new Loan(this.amount(),this.nbMonth(), this.annualRate()))
+  })
+
+  logLoanAmount = effect(()=>{console.log("nouvelle valeur de loan=" + JSON.stringify(this.sLoan()))})
 
   monthlyPayment=computed(()=>this.financialService.monthlyPaymentValue(this.nbMonth(),this.amount(),this.annualRate()));
 

375bbde069cd2b3c7ffff6e0adc9e563fab85250
Didier Defrance <didier@d-defrance.fr>
2026-06-03 15:52:27 +0200

    s

--- tp/debut_juin2026/my-nx-workspace/apps/my-app/src/app/common/service/financial.service.spec.ts
+++ tp/debut_juin2026/my-nx-workspace/apps/my-app/src/app/common/service/financial.service.spec.ts
@@ -15,7 +15,16 @@ describe('FinancialService', () => {
     console.log("FinancialService created");
   });
 
+   test('pour amount=100000 , taux=2.5 , nbMonth=120 , mensualite = 943', () => {
+      const mensualite = service.monthlyPaymentValue(120,100000,2.5);
+      console.log("mensualite="+mensualite)
+     expect(mensualite).toBeCloseTo(942.7,1)
+  });
+
  
 });
+
+//ng test --include=**/financial.service.spec.ts
+//nx test my-app --include=**/financial.service.spec.ts
 
 
